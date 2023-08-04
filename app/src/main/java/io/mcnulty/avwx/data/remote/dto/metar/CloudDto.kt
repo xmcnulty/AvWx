100dcc7421893f71497842b5236bf5cd8642208b
xmcnulty <x.mcnulty@gmail.com>
2023-08-04 00:35:08 -0400

    Created CloudParserTest.

--- app/src/main/java/io/mcnulty/avwx/data/remote/dto/metar/CloudDto.kt
+++ app/src/main/java/io/mcnulty/avwx/data/remote/dto/metar/CloudDto.kt
@@ -1,5 +1,21 @@
 package io.mcnulty.avwx.data.remote.dto.metar
 
+/**
+ * Data class representing cloud JSON object returned from AVWX API.
+ *
+ * Sample JSON:
+ *     {
+ *       "repr": "SCT022CB",
+ *       "type": "SCT",
+ *       "altitude": 22,
+ *       "modifier": "CB"
+ *     }
+ *
+ * @property altitude The cloud base altitude in hundreds of feet
+ * @property modifier The cloud modifier. Typically cloud type.
+ * @property repr The raw cloud string
+ * @property type The cloud type
+ */
 data class CloudDto(
     val altitude: Int,
     val modifier: String,
